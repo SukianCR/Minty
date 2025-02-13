92efc32a1936d16cf3bcb6f2bc4f730b798af8fc
sukimixcr <sukimixcr@gmail.com>
2025-02-13 18:38:44 -0500

    level 1 landing page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -12,31 +12,33 @@ export default function Home() {
       <div className="p-2 bg-light border">
         Susana's Web Developer Demo which includes :
       </div>
-      <div className="p-2 bg-light border ">
+
+      <div className="p-2 bg-light  ">
         <ul className="home">
-          <li>
+          <li className="border border-secondary ">
             {" "}
-            <i className="bi bi-feather pr-05 text-secondary"></i> Design your
-            own garden with js library dnd for drag and drop.
+            <i className="bi bi-feather  text-secondary pr-05 "></i> Design your
+            own garden with js library dnd kit for drag and drop.
           </li>
-          <li>
+          <li className="border border-primary ">
             {" "}
             <i className="bi bi-feather pr-05 text-primary"></i> Filter artists
             with the costarican artists directory . Using register, login and
             jswebtoken for authenticating.{" "}
           </li>
-          <li>
+          <li className="border border-info ">
             {" "}
             <i className="bi bi-feather pr-05 text-info"></i> Main projects on
             my FullStack Academy web developer bootcamp{" "}
           </li>
-          <li>
+          <li className="border border-warning ">
             {" "}
             <i className="bi bi-feather pr-05 text-warning "></i>About Me/
             Resume / Contact
           </li>
         </ul>
       </div>
+
       <div className="p-2 bg-light border ">
         Technologies used: PERN Postgress , Express , React and Node
       </div>
